5ddbe1f8758a8de8e9d4ea4467706da05c73b536
Kyle Jones <kyjo1308@gmail.com>
2023-06-09 22:43:27 +0100

    Changed file structure

---
+++ RayTracer/src/Color.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "Vec3.h"
+#include <iostream>
+
+void PrintColor(std::ostream& os, vec3 pixelColor)
+{
+	// Write the translated [0, 255] value of each color channel.
+	os << static_cast<int>(255.0f * pixelColor.r()) << ' '
+		<< static_cast<int>(255.0f * pixelColor.g()) << ' '
+		<< static_cast<int>(255.0f * pixelColor.b()) << '\n';
+}
